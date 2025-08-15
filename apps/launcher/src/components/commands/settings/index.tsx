37edc91cad5ea2504cbb66e8c7dc809e13f9d355
Riley Barabash <admin@rileybarabash.com>
2025-08-15 11:21:19 -0600

    minified settings

--- apps/launcher/src/components/commands/settings/index.tsx
+++ apps/launcher/src/components/commands/settings/index.tsx
@@ -7,7 +7,7 @@ import { Form } from "@raycast/api"
 export default function Settings() {
     return (
         <Form>
-            <Form.Checkbox id="auto-upload-voice-captures" label="Auto-Upload Voice Captures" defaultValue={true} />
+            <Form.Description title="Placeholder" text="Nothing here yet." />
         </Form>
     )
 }
